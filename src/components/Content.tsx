86a0895e111aca69e5ae7f46bc9c794889f14c6f
Saullo Bueno <saullobueno@gmail.com>
2022-03-29 00:36:29 -0300

    Solution finished

--- src/components/Content.tsx
+++ src/components/Content.tsx
@@ -1,3 +1,50 @@
-export function Content() {
-  // Complete aqui
+import { useEffect, useState } from 'react';
+import { Header } from '../components/Header';
+import { MovieCard } from '../components/MovieCard';
+import { api } from '../services/api';
+
+interface MovieProps {
+	imdbID: string;
+	Title: string;
+	Poster: string;
+	Ratings: Array<{
+		Source: string;
+		Value: string;
+	}>;
+	Runtime: string;
+}
+
+interface ContentProps {
+	selectedGenreId: number;
+}
+export function Content({ selectedGenreId }: ContentProps) {
+	const [movies, setMovies] = useState<MovieProps[]>([]);
+
+	useEffect(() => {
+		api
+			.get<MovieProps[]>(`movies/?Genre_id=${selectedGenreId}`)
+			.then((response) => {
+				setMovies(response.data);
+			});
+	}, [selectedGenreId]);
+
+	return (
+		<div className="container">
+			<Header selectedGenreId={selectedGenreId} />
+
+			<main>
+				<div className="movies-list">
+					{movies?.map((movie) => (
+						<MovieCard
+							key={movie.imdbID}
+							title={movie.Title}
+							poster={movie.Poster}
+							runtime={movie.Runtime}
+							rating={movie.Ratings[0].Value}
+						/>
+					))}
+				</div>
+			</main>
+		</div>
+	);
 }
